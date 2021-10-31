ed88285eb7412b7cfe8be3ac0dd5294bc4948e2a
Reid <rlj0713@gmail.com>
2021-10-31 14:53:51 -0400

    moved folders and added react for frontend

---
+++ backend/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+
+  get '/auth/:provider/callback' => 'google#create'
+
+  get "signup" => 'users#new'
+  get "login" => 'sessions#new'
+
+  post '/sessions' => 'sessions#create'
+  delete '/sessions' => 'sessions#destroy'
+  
+  resources :users
+  resources :customers
+  resources :guides
+  resources :reservations 
+end
